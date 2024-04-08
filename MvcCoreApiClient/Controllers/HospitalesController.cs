82eacbcde3441351c632b2a47a49c32608ab6593
anitapm03 <apereiramolinero@gmail.com>
2024-04-08 09:32:11 +0200

    uwu

--- MvcCoreApiClient/Controllers/HospitalesController.cs
+++ MvcCoreApiClient/Controllers/HospitalesController.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiClient.Models;
+using MvcCoreApiClient.Services;
 
 namespace MvcCoreApiClient.Controllers
 {
     public class HospitalesController : Controller
     {
+        private ServiceHospital service;
+
+        public HospitalesController(ServiceHospital service) 
+        { 
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Servidor()
+        {
+            List<Hospital> hospitals =
+                await this.service.GetHospitalesAsync();
+            return View(hospitals); 
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ MvcCoreApiClient/Services/ServiceHospital.cs
@@ -0,0 +1,63 @@
+﻿using MvcCoreApiClient.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MvcCoreApiClient.Services
+{
+    public class ServiceHospital
+    {
+
+        //clase para indicar el formato que estamos consumiendi
+        private MediaTypeWithQualityHeaderValue header;
+
+        //nuestra URL del servicio
+        private string ApiUrl;
+
+        public ServiceHospital()
+        {
+            this.ApiUrl =
+                "https://apinetcorehospitalespaco.azurewebsites.net/";
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        //creamos un metodo asincrono para leer hospitales
+        public async Task<List<Hospital>>
+            GetHospitalesAsync()
+        {
+            //usamos la clase Httpclient para las peticiones
+            using (HttpClient client = new HttpClient())
+            {
+                //necesitamos la peticion
+                string request = "api/hospitales";
+                //indicamos la url base de nuestro servicio
+                client.BaseAddress = new Uri(this.ApiUrl);
+                //como norma debemos limpiar las cabeceras en 
+                //cada peticion por si en algun momento las 
+                //mezclamos y nos daria error
+                client.DefaultRequestHeaders.Clear();
+                //indcamos el tipo de consulta a consumir
+                client.DefaultRequestHeaders.Accept.Add
+                    (this.header);
+                //realizamos la peticion y almacenamos 
+                //los resultados en una respuesta
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if(response.IsSuccessStatusCode)
+                {
+                    //primero vamos a realizar la peticion usando
+                    //newtonsoft para que tengais un ejemplo si las
+                    //propiedades del json y del model no fueran iguales
+                    string json =
+                        await response.Content.ReadAsStringAsync();
+                    List<Hospital> data =
+                        JsonConvert.DeserializeObject<List<Hospital>>(json);
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
